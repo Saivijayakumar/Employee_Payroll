67bf738f9c56cfb772361df49b2f9ead6c4ff15b
Sai Vijayakumar Bollu <saivijayakumar.b@tvsnext.io>
2021-08-01 16:01:38 +0530

    [SaiVijayaKumar]Inserting values for coloums

--- payroll.sql
+++ payroll.sql
@@ -10,3 +10,7 @@ Salary float not null,
 StartDate date
 );
 --For delete a table =>drop table employee_payroll;
+----------UC3---------
+insert into employee_payroll(Name,StartDate,Salary) values ('Ram','2020-09-3',19448),('Ravi','2010-08-3',99448),('sai','2021-06-21',909448);
+----Used to delete all rows in table -->delete from employee_payroll;
+----Used to delete specified rows in table -->delete from employee_payroll where condition;
