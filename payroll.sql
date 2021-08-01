993fb8fc548159e15fccf382a6329a9d7fc3cb7a
Sai Vijayakumar Bollu <saivijayakumar.b@tvsnext.io>
2021-08-01 15:59:02 +0530

    [SaiVijayaKumar] Creating Table

--- payroll.sql
+++ payroll.sql
@@ -1,3 +1,12 @@
 ---------UC1--------------
 create database payroll_services;
 use payroll_services;
+----------UC2------------
+create table employee_payroll
+(
+Id int identity(1,1) primary key,
+Name varchar(200) not null,
+Salary float not null,
+StartDate date
+);
+--For delete a table =>drop table employee_payroll;
